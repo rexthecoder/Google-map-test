17edeb43056df0f3bbfd3dcf3eb437e52ca20e5f
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2024-02-25 23:06:01 +0000

    chore: removed api

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDZqsDwbMjN-77YKlGZ57v5z8QI_Ct5NTY")
+    // TODO: Add your Google Maps API Key
+    GMSServices.provideAPIKey('')
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
